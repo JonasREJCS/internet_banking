f6df80a636ba8250bdcce0b1e1310ff42c4ef3c2
JonasREJCS <jj_jcs@hotmail.com>
2019-11-25 16:26:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./assets/logo.svg";
+import Emoji from "./emoji/index";
 
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault()
+    
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} width="90px" height="90px" alt="Internet Banking" />
+      <div className="content">
+        <p>Efetue operações bancárias com simplicidade</p>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Emoji label="sheep" symbol="🌐💰" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            type="email"
+            id="email"
+            placeholder="Digite seu melhor email"
+          />
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
