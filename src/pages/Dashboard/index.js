24b2a90893709c122ada6edbee623fed09b090c9
JonasREJCS <jj_jcs@hotmail.com>
2019-11-26 15:17:48 -0300

    adicionado card para indicar a funcao de credito no futuro

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 // import SaldoAtual from "../../components/SaldoAtual/index";
-import './index.css'
+import "./index.css";
 
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, CardDeck } from "react-bootstrap";
 
 export default function Dashboard({ history }) {
   const usuario = localStorage.getItem("user");
@@ -15,16 +15,29 @@ export default function Dashboard({ history }) {
   return (
     <>
       <p>Bem vindo, {usuario} !</p>
-      <Card bg="light" className="text-center" style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>Conta Corrente</Card.Title>
-          <Card.Subtitle>Saldo disponível</Card.Subtitle>
-          <Card.Text>R$ 25.000,00</Card.Text>
-          <Button variant="primary" onClick={onClick}>
-            Ver extrato
-          </Button>
-        </Card.Body>
-      </Card>
+      <CardDeck>
+        <Card bg="light" className="text-center" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Conta Corrente</Card.Title>
+            <Card.Subtitle>Saldo disponível</Card.Subtitle>
+            <Card.Text>R$ 25.000,00</Card.Text>
+            <Button variant="primary" onClick={onClick}>
+              Ver extrato
+            </Button>
+          </Card.Body>
+        </Card>
+        <Card bg="light" className="text-center" style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Cartão de Crédito</Card.Title>
+            <Card.Subtitle>Em breve você poderá solicitar nosso cartão de crédito</Card.Subtitle>
+            <img alt="visa logo" width="50px" src={require('../../assets/visa_logo.jpg')} />
+            <img alt="mastercard logo" width="50px" src={require('../../assets/mastercard_logo.jpg')} />
+            <Button variant="dark" disabled="disabled">
+              Solicitar Cartão (DISPONÍVEL EM BREVE)
+            </Button>
+          </Card.Body>
+        </Card>
+        </CardDeck>
     </>
   );
 }
