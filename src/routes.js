f1e0581fd67a41e41fc9075f1d7c69bd88d84a73
JonasREJCS <jj_jcs@hotmail.com>
2019-11-26 13:19:46 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -5,21 +5,11 @@ import Dashboard from './pages/Dashboard'
 import Extrato from './pages/Extrato';
 
 export default function Routes() {
-    function requireAuth(nextState, replace, next) {
-        if (!localStorage.getItem('user')) {
-          replace({
-            pathname: "/login",
-            state: {nextPathname: nextState.location.pathname}
-          });
-        }
-        next();
-      }
-
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login} onEnter={requireAuth}/>
-                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/" exact component={Login} />
+                <Route path="/dashboard" component={Dashboard}/>
                 <Route path="/extrato" component={Extrato} />
             </Switch>
         </BrowserRouter>
